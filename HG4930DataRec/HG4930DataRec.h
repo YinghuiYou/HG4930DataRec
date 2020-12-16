41d7a947978aececf7293a94be124af60f8ddc14
Ying You <1575856209@qq.com>
2020-12-16 10:51:27 +0800

    完成了界面设计添加 .gitignore 和 .gitattributes。

---
+++ HG4930DataRec/HG4930DataRec.h
@@ -0,0 +1,32 @@
+﻿
+// HG4930DataRec.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CHG4930DataRecApp:
+// 有关此类的实现，请参阅 HG4930DataRec.cpp
+//
+
+class CHG4930DataRecApp : public CWinApp
+{
+public:
+	CHG4930DataRecApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CHG4930DataRecApp theApp;
---
+++ HG4930DataRec/HG4930DataRecDlg.h
@@ -0,0 +1,34 @@
+﻿
+// HG4930DataRecDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CHG4930DataRecDlg 对话框
+class CHG4930DataRecDlg : public CDialogEx
+{
+// 构造
+public:
+	CHG4930DataRecDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_HG4930DATAREC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
